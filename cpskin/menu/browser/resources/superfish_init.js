045d2ffb13fe6155822342ad77fb5e8b5fc0281d
Luca Bellenghi <lucabel@gmail.com>
2014-05-14 15:11:39 +0200

    some change for mobile version

--- cpskin/menu/browser/resources/superfish_init.js
+++ cpskin/menu/browser/resources/superfish_init.js
@@ -1,8 +1,25 @@
 /* initialise plugins */
 ;(function($){
     "use strict";
-
     $( document ).ready(function() {
-        $('ul.sf-menu').superfish();
+        var $isMobile = Detectizr.device.type == 'mobile';
+        if (!$isMobile){
+            $('ul.sf-menu').superfish();
+        }
+        else{
+            var $$ = $('ul.sf-menu');
+            var $liHasUl = $$.find('li:has(ul)');
+            var $mobileArrow = $('<div class="mobnav-subarrow"></div>');
+            $liHasUl.find('> span').after($mobileArrow.clone());
+
+            $('#mobnav-btn').click(
+                function () {
+                    $('.sf-menu').toggleClass("xactive");
+                });
+            $('.mobnav-subarrow').click(
+                function () {
+                    $(this).parent().toggleClass("xpopdrop");
+                });
+        }
     });
 })(jQuery);
