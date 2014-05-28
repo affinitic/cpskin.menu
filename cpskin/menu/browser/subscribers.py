98b7226df405c86d45e82b5583b83ca4b4ece640
Jean-François Roche <jfroche@affinitic.be>
2014-05-29 01:59:22 +0200

    if package is not installed, do not invalidate anything

--- cpskin/menu/browser/subscribers.py
+++ cpskin/menu/browser/subscribers.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
+from zope.component.hooks import getSite
+from plone.uuid.interfaces import IUUID
 from cpskin.menu.browser.menu import invalidate_menu
 
 
 def content_modified(content, event):
-    invalidate_menu(content)
+    if IUUID(getSite(), None) is not None:
+        invalidate_menu(content)
