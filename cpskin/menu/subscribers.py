f94cc863d8d4923cfda1a7aee3b0c436c3ac07b0
Jean-François Roche <jfroche@affinitic.be>
2014-08-11 17:42:59 +0200

    also verify if the object is acquisition wrapped before invalidating the menu on it

--- cpskin/menu/subscribers.py
+++ cpskin/menu/subscribers.py
@@ -1,13 +1,27 @@
 # -*- coding: utf-8 -*-
 from zope.component.hooks import getSite
+from Acquisition import aq_chain
 from plone.uuid.interfaces import IUUID
 from cpskin.menu.browser.menu import invalidate_menu
 
 
-def content_modified(content, event):
+def content_has_id(content):
     try:
         content.getId()
     except AttributeError:
+        return False
+    else:
+        return True
+
+
+def object_is_wrapped(content):
+    return len(aq_chain(content)) > 1
+
+
+def content_modified(content, event):
+    if not content_has_id(content):
+        return
+    if not object_is_wrapped(content):
         return
     if IUUID(getSite(), None) is not None:
         invalidate_menu(content)
