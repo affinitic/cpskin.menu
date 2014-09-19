7a23d37e2743bb0507a0973c7e69304c5b19977d
schminitz <gillian@affinitic.be>
2014-09-19 09:42:24 +0200

    Add upgrade step for cpskin_menu.js affinitic refs #5749

--- cpskin/menu/upgrades.py
+++ cpskin/menu/upgrades.py
@@ -3,3 +3,7 @@ PROFILE_ID = 'profile-cpskin.menu:default'
 
 def upgrade_1000_to_1001(context):
     context.runImportStepFromProfile(PROFILE_ID, 'cssregistry')
+
+
+def upgrade_1001_to_1002(context):
+    context.runImportStepFromProfile(PROFILE_ID, 'jsregistry')
