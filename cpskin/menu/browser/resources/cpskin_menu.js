dfa3170d4c0bae4b51cf59d0869f93f2042f8c4c
schminitz <gillian@affinitic.be>
2014-09-26 13:37:15 +0200

    Add 'activated' class on opened menu affinitic refs #5749

--- cpskin/menu/browser/resources/cpskin_menu.js
+++ cpskin/menu/browser/resources/cpskin_menu.js
@@ -1,12 +1,21 @@
 $( document ).ready(function() {
 
-    $('#portal-globalnav li:not(.menutools-item):not(#portaltab-index_html) a').each(function(){
+    // Add 'activated' class to menu already loaded
+    $('#portal-globalnav li.selected:not(.menutools-item):not(#portaltab-index_html)').addClass('activated');
+
+    // Toggle between menu clicked
+    var clickable_menu_selector = '#portal-globalnav li:not(.menutools-item):not(#portaltab-index_html) a';
+    $(clickable_menu_selector).each(function(){
         $(this).click(function() {
+
+            $(clickable_menu_selector).each(function(){
+                $(this).removeClass('activated');
+            });
+            $(this).addClass('activated');
 
             $('ul.sf-menu').each(function(){
                 $(this).hide();
             });
-
             var menu_id = this.parentNode.id.replace('portaltab-', '');
             jQuery('#portal-globalnav-cpskinmenu-' + menu_id).show();
             return false;
